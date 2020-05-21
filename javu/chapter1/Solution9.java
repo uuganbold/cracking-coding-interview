66ccf5c1036388e18eae5bed70aff34b2d4ed29e
uuganbold <uuganhuu@gmail.com>
2020-05-21 01:55:16 -0500

    question 1.9

---
+++ javu/chapter1/Solution9.java
@@ -0,0 +1,17 @@
+package javu.chapter1;
+
+public class Solution9 {
+    
+    /*
+        T: O(N)
+        S: O(N)
+    */
+    public static boolean isRotation(String str, String str2){
+         String newStr=str+str;
+         return newStr.indexOf(str2)>=0;
+    }
+
+    public static void main(String[] args){
+        System.out.println(isRotation("waterbottle", "erbottlewat"));
+    }
+}
