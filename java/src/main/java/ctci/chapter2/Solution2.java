8da5a3500ca06104a4872cad8307435e4f817afc
uuganbold <uuganhuu@gmail.com>
2020-06-01 01:05:46 -0500

    addition solution to exercise 2.2

--- java/src/main/java/ctci/chapter2/Solution2.java
+++ java/src/main/java/ctci/chapter2/Solution2.java
@@ -5,17 +5,16 @@ public class Solution2 {
      *  Is it guaranteed that the list is longer than k.
      * What if it is smaller than K? return null or first node?
      */
-
      /*
         T: O(N)
         S: O(1)
      */
      public static Integer kthLastNode(ListNode head, int k){
-         if(head==null) return null;
-         assert k>0;
+        if(head==null) return null;
+        assert k>0;
 
-         ListNode slow=head;
-         ListNode fast=head;
+        ListNode slow=head;
+        ListNode fast=head;
 
          while(k>0&&fast!=null){
              fast=fast.next;
@@ -30,6 +29,22 @@ public class Solution2 {
          }
 
          return slow.data;
+     }
+
+    /**
+     * T: O(N)
+     * S: O(N)
+     */
+     static int value; 
+     private static int kthLastNodeRecurse(ListNode head, int k){
+         if(head==null){
+             return 0;
+         }
+         int prev=kthLastNode(head.next,k);
+         if(prev==k) {
+             value=k;
+         }
+         return prev+1;
      }
 
 }
